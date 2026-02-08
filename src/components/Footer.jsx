0f369bb43eea0512de66c47cbddb19d5842ba21a
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2026-02-08 18:44:53 +0530

    Added real links for contacts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ import { assets } from "../assets/assets";
 const Footer = () => {
   return (
     <footer className="w-full bg-zinc-950/60 backdrop-blur-md border-t border-zinc-800 px-6 sm:px-16 py-6 flex sm:flex-row items-center justify-between gap-4 text-white">
-      
       <div className="hidden sm:flex items-center gap-2">
         <img
           src={assets.logo_icon}
@@ -21,19 +20,22 @@ const Footer = () => {
 
       <div className="flex gap-3 mt-3 sm:mt-0">
         <a
-          href="#"
+          href="https://www.facebook.com/profile.php?id=61587858477840"
+          target="_blank"
           className="w-9 h-9 flex items-center justify-center rounded-full bg-zinc-800 hover:bg-zinc-700 transition"
         >
           <FaFacebookF className="text-white w-5 h-5" />
         </a>
         <a
-          href="#"
+          href="https://www.instagram.com/crexo.ai_26"
+          target="_blank"
           className="w-9 h-9 flex items-center justify-center rounded-full bg-zinc-800 hover:bg-zinc-700 transition"
         >
           <FaInstagram className="text-white w-5 h-5" />
         </a>
         <a
-          href="#"
+          href="mailto:crexo.ai@gmail.com"
+          target="_blank"
           className="w-9 h-9 flex items-center justify-center rounded-full bg-zinc-800 hover:bg-zinc-700 transition"
         >
           <FaEnvelope className="text-white w-5 h-5" />
